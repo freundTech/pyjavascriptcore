ff1742b8a9f76f77764b4b7af64aef9b5d4dfedb
Martín Soto <soto@masoft>
2009-10-25 00:32:33 +0200

    Initial test cases for attribute access
    * test_javascriptcore.py (EvaluateScriptTestCase.tearDown): Use del
      instead of setting he attribute to None.
      (EvaluateScriptTestCase): New test.
      (ContextLiveTestCase, AttributeAccessTestCase): New test classes.

--- test_javascriptcore.py
+++ test_javascriptcore.py
@@ -25,14 +25,14 @@ import javascriptcore as jscore
 
 
 class EvaluateScriptTestCase(unittest.TestCase):
-    """Create a context, evaluate scripts in it and check their return
-    values."""
+    """Evaluate arbitrary expressions in the JavaScript interpreter.
+    """
 
     def setUp(self):
         self.ctx = jscore.JSContext()
 
     def tearDown(self):
-        self.ctx = None
+        del self.ctx
 
     def testEvaluateInt1(self):
         self.assertEqual(self.ctx.evaluateScript('3 + 2'), 5)
@@ -48,12 +48,82 @@ class EvaluateScriptTestCase(unittest.TestCase):
     def testEvaluateString1(self):
         self.assertEqual(self.ctx.evaluateScript('"a"'), 'a')
 
+    def testEvaluateObject1(self):
+        # Object initializers are not expressions, the parenthesis
+        # create an expression, though.
+        obj = self.ctx.evaluateScript("""({a: 1, b: 'x'})""")
+        self.assertEqual(obj.a, 1)
+        self.assertEqual(obj.b, 'x')
+
     def testEvaluateError(self):
         def code():
             program = '(function(x){return x+2 return})(3)'
             self.assertEqual(self.ctx.evaluateScript(program), 5)
 
         self.assertRaises(jscore.JSException, code)
+
+
+class ContextLiveTestCase(unittest.TestCase):
+    """Check that the context remains alive when Python still
+    references some of its objects.
+    """
+
+    def testContextLive(self):
+        ctx = jscore.JSContext()
+        obj = ctx.evaluateScript("""({a: 1, b: 'x'})""")
+
+        # Release the Python context object.
+        del ctx
+
+        # Check that the object is still useful.
+        self.assertEqual(obj.a, 1)
+        self.assertEqual(obj.b, 'x')
+
+        # Release the object.
+        del obj
+
+
+class AttributeAccessTestCase(unittest.TestCase):
+    """Access the attributes of JavaScript objects from Python
+    """
+
+    def setUp(self):
+        self.ctx = jscore.JSContext()
+        self.obj = self.ctx.evaluateScript("""
+          obj = {a: 1,
+                 b: 'x',
+                 c: {d: 2,
+                     e: 'yy'}};
+          obj;
+          """)
+
+    def tearDown(self):
+        del self.ctx
+
+    def testIntAccess(self):
+        self.assertEqual(self.obj.a, 1)
+
+    def testStringAccess(self):
+        self.assertEqual(self.obj.b, 'x')
+
+    def testNestedObjectAccess(self):
+        self.assertEqual(self.obj.c.d, 2)
+        self.assertEqual(self.obj.c.e, 'yy')
+
+    def testAccessJSChanged(self):
+        self.assertEqual(self.obj.a, 1)
+        self.ctx.evaluateScript('obj.a = 4')
+        self.assertEqual(self.obj.a, 4)
+
+    def testAccessJSNew(self):
+        self.ctx.evaluateScript('obj.f = 4')
+        self.assertEqual(self.obj.f, 4)
+
+    def testAccessError(self):
+        def code():
+            print self.obj.abc
+
+        self.assertRaises(AttributeError, code)
 
 
 if __name__ == '__main__':
